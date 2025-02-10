638cc046f50c6f0e2e38a9e6bcf1f80868c3e2af
FabioGunderson <fmg44@scarletmail.rutgers.edu>
2025-02-10 22:43:53 +0000

    hw2 fix: correct code for question 2 and 3 from chapter 3

--- homeworks/hw2/source_files/hw2_question2_conditionalsignal.vhd
+++ homeworks/hw2/source_files/hw2_question2_conditionalsignal.vhd
@@ -22,17 +22,21 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity hw2_question2_conditionalsignal is
+entity hw_2_question_2_selectedsignal is
     port (
         A, B : in std_logic;  -- Inputs
         F    : out std_logic  -- Output
     );
-end hw2_question2_conditionalsignal;
+end hw_2_question_2_selectedsignal;
 
-architecture conditional of hw2_question2_conditionalsignal is
+architecture selected of hw_2_question_2_selectedsignal is
+    signal AB : std_logic_vector(1 downto 0);  -- Concatenated signal
 begin
-    F <= '1' when (not A and B) = '1' else   -- A'B
-         '1' when (A = '1') else             -- A
-         '1' when (A and not B) = '1' else   -- AB'
-         '0';                                -- Default case (F = 0)
-end conditional;
+    AB <= A & B;  -- Concatenation of A and B into a 2-bit vector
+
+    with AB select
+        F <= '1' when "01",  -- A'B
+             '1' when "10",  -- AB'
+             '1' when "11",  -- A
+             '0' when "00";  -- Default case
+end selected;
--- homeworks/hw2/source_files/hw2_question3_selectedsignal.vhd
+++ homeworks/hw2/source_files/hw2_question3_selectedsignal.vhd
@@ -21,12 +21,23 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity hw2_question3_selectedsignal is
---  Port ( );
+    port (
+        D    : in std_logic;
+        SEL  : in std_logic_vector(2 downto 0);
+        Y    : out std_logic_vector(7 downto 0)
+    );
 end hw2_question3_selectedsignal;
 
-architecture Behavioral of hw2_question3_selectedsignal is
-
+architecture selected of hw2_question3_selectedsignal is
 begin
-
-
-end Behavioral;
+    with SEL select
+        Y <= "00000001" when "000",
+             "00000010" when "001",
+             "00000100" when "010",
+             "00001000" when "011",
+             "00010000" when "100",
+             "00100000" when "101",
+             "01000000" when "110",
+             "10000000" when "111",
+             "00000000" when others;  -- Default case
+end selected;
